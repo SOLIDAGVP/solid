ec20c67226a627d24f2b232af7df2539ba842cf0
Administrador <Administrador@GGIECUT1WRK013.espol.edu.ec>
2018-10-30 13:27:06 -0500

    Creación de paquetes bien

---
+++ solid/src/_3isp/bien/EntrenamientoPokemon.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package _3isp.bien;
+
+import _3isp.mal.*;
+
+/**
+ * 
+ * @author Palacios
+ */
+
+/*
+    Esta es una "interfaz gorda" ya que tiene muchas responsabilidades
+*/
+public interface EntrenamientoPokemon {
+    
+    void aprenderAtaqueAgua();
+    void aprenderAtaqueFuego();
+    void aprenderAtaqueCorte();   //todo pokemon tiene esta habilidad
+    
+    
+}
